3aba5b7d604fb04a02e06042be5f4b6bd12a16dc
Brendan <meme@Meme.com>
2022-06-07 21:20:11 -0700

    meme2

--- src/app/services/utility.ts
+++ src/app/services/utility.ts
@@ -1,6 +1,6 @@
 export default class utility {
     //private static url: String = "http://192.168.0.34:8080/";
-    private static url: String = "http://http://68.104.238.128:9994/";
+    private static url: String = "http://68.104.238.128:9994/";
 
     public static getUrl() {
         return this.url;
